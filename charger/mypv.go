0b8783254685a3b1991713cf915780643bf4556d
Oliver Merk <oliver.merk@gmx.de>
2025-08-29 14:10:09 +0200

    wip

--- charger/mypv.go
+++ charger/mypv.go
@@ -91,11 +91,11 @@ func newMyPvFromConfig(ctx context.Context, name string, other map[string]interf
 		return nil, err
 	}
 
-	return NewMyPv(ctx, name, cc.URI, cc.ID, cc.TempSource, statusC, cc.Scale)
+	return NewMyPv(ctx, name, cc.URI, cc.ID, cc.TempSource, statusC, cc.Scale, cc.RelaisHeaterPower)
 }
 
 // NewMyPv creates myPV AC Elwa 2 or Thor charger
-func NewMyPv(ctx context.Context, name, uri string, slaveID uint8, tempSource int, statusC uint16, scale float64) (api.Charger, error) {
+func NewMyPv(ctx context.Context, name, uri string, slaveID uint8, tempSource int, statusC uint16, scale float64, relaisheaterpower uint32) (api.Charger, error) {
 	conn, err := modbus.NewConnection(ctx, uri, "", "", 0, modbus.Tcp, slaveID)
 	if err != nil {
 		return nil, err
